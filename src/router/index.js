cfaac22de146454ad5827d9d8981d79cfd3d6a88
k24 <winsuntom@163.com>
2017-06-29 18:13:09 +0800

    版本修复

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import { base } from '@/utils/config'
 
 import ChartEdit from '@/views/ChartEdit4'
+import widgetList from '@/views/widgetList';
 import Edit from '@/views/Echarts/edit';
-import BraceCharts from '@/views/BraceCharts'
-import DataSetDefine from '@/views/DataSetDefinition/index'
+import BraceCharts from '@/views/Echarts/widget'
+import DataSetDefine from '@/views/DataSetDefinition'
 import HandsonTable from '@/views/DataTable'
 
 Vue.use(Router)
@@ -28,12 +29,16 @@ export default new Router({
       path: '/widgetDesigner',
       name: 'braceCharts',
       component: BraceCharts,
-    },
+    },{
+      path: '/widgetList',
+      name: 'widgetList',
+      component: widgetList,
+    }/*,
     {
       path: '/data_def',
       name: 'braceCharts',
       component: DataSetDefine,
-    },/*
+    },
     {
       path:'/table',
       name:'HandsonTable',
