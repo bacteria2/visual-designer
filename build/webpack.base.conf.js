3f058085ee598c51d88366cb1af5921e9a2e8c7f
Lee <527138445@qq.com>
2017-07-04 20:07:49 +0800

    /@-@/ 在store中 添加 dashbord

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,6 +23,26 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'jquery-ui/data':'jquery-ui/ui/data',
+      'jquery-ui/disable-selection':'jquery-ui/ui/disable-selection',
+      'jquery-ui/focusable':'jquery-ui/ui/focusable',
+      'jquery-ui/form':'jquery-ui/ui/form',
+      'jquery-ui/ie':'jquery-ui/ui/ie',
+      'jquery-ui/keycode':'jquery-ui/ui/keycode',
+      'jquery-ui/jquery-1-7':'jquery-ui/ui/jquery-1-7',
+      'jquery-ui/plugin':'jquery-ui/ui/plugin',
+      'jquery-ui/safe-active-element':'jquery-ui/ui/safe-active-element',
+      'jquery-ui/safe-blur':'jquery-ui/ui/safe-blur',
+      'jquery-ui/scroll-parent':'jquery-ui/ui/scroll-parent',
+      'jquery-ui/tabbable':'jquery-ui/ui/tabbable',
+      'jquery-ui/unique-id':'jquery-ui/ui/unique-id',
+      'jquery-ui/version':'jquery-ui/ui/version',
+      'jquery-ui/widget':'jquery-ui/ui/widget',
+      'jquery-ui/labels':'jquery-ui/ui/labels',
+      'jquery-ui/widgets/mouse':'jquery-ui/ui/widgets/mouse',
+      'jquery-ui/widgets/draggable':'jquery-ui/ui/widgets/draggable',
+      'jquery-ui/widgets/droppable':'jquery-ui/ui/widgets/droppable',
+      'jquery-ui/widgets/resizable':'jquery-ui/ui/widgets/resizable'
     }
   },
   module: {
--- src/views/DashBord/Dashbord.vue
+++ src/views/DashBord/Dashbord.vue
@@ -8,7 +8,7 @@
          :data-gs-x="layout.x" :data-gs-y="layout.y"
          :data-gs-width="layout.w" :data-gs-height="layout.h">
       <div class="grid-stack-item-content">
-        <char-container id="abc"></char-container>
+        <!--<char-container id="abc"></char-container>-->
       </div>
     </div>
 
@@ -17,8 +17,28 @@
  </template>
 
 <style scoped lang="scss">
-  div {
-    border:1px solid #999;
+  .grid-stack-item{
+    text-align: center;
+    background-color: #ffffff;
+    -webkit-border-top-right-radius: 5px;
+    -webkit-border-bottom-right-radius: 5px;
+    -webkit-border-bottom-left-radius: 5px;
+    -webkit-border-top-left-radius: 5px;
+    -moz-border-radius-topright: 5px;
+    -moz-border-radius-bottomright: 5px;
+    -moz-border-radius-bottomleft: 5px;
+    -moz-border-radius-topleft: 5px;
+    border-radius: 5px;
+    -webkit-background-clip: padding-box;
+    background-clip: padding-box;
+    border: solid 1px #31B0D5;
+    -webkit-box-shadow: 0 1px 2px #31B0D5;
+    -moz-box-shadow: 0 1px 2px #31B0D5;
+    box-shadow: 0 1px 2px #31B0D5;
+  }
+  .grid-stack-highlight{
+    border:1px dotted red;
+    box-shadow:none;
   }
 
 </style>
@@ -26,9 +46,10 @@
 
     import store from '@/store'
     import CharContainer from '@/components/CharContainer'
-    import $ from 'jquery'
-    import gridstack from 'gridstack'
-    import ui from 'jquery-ui'
+//    import $ from 'jquery/dist/jquery.min'
+    import 'gridstack/dist/gridstack.css'
+    import $ from 'gridstack/dist/gridstack.all'
+
 
     export default {
       components:{
@@ -36,7 +57,11 @@
       },
       mounted(){
         $(function () {
-          $('.grid-stack').gridstack();
+            let options = {
+              cellHeight: 60,
+              verticalMargin: 10
+            };
+            $('.grid-stack').gridstack(options);
         });
       },
       data(){
@@ -48,7 +73,7 @@
       methods:{
         addBord(){
             let id = Math.random().toString(36).substr(2,32);
-            let newLayout = {x:0,y:0,w:1,h:1,id:id,container:{}};
+            let newLayout = {x:1,y:2,w:4,h:4,id:id,container:{}};
             store.commit('addDashbordLayout',newLayout);
         }
       }
