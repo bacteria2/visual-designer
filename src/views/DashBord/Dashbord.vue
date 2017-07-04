aebcd11503488e6322c17fa61784ab5ed63b6901
Lee <527138445@qq.com>
2017-07-04 15:28:40 +0800

    /@-@/ 在store中 添加 dashbord

--- src/views/DashBord/Dashbord.vue
+++ src/views/DashBord/Dashbord.vue
@@ -23,14 +23,12 @@
 
 </style>
   <script type="text/javascript">
-    import Vue from 'vue'
+
     import store from '@/store'
     import CharContainer from '@/components/CharContainer'
     import $ from 'jquery'
     import gridstack from 'gridstack'
     import ui from 'jquery-ui'
-
-    Vue.component(CharContainer.name,CharContainer);
 
     export default {
       components:{
@@ -49,10 +47,9 @@
       },
       methods:{
         addBord(){
-            let id = Math.random();
-            let newLayout = {x:0,y:0,w:1,h:1,};
+            let id = Math.random().toString(36).substr(2,32);
+            let newLayout = {x:0,y:0,w:1,h:1,id:id,container:{}};
             store.commit('addDashbordLayout',newLayout);
-
         }
       }
     }
