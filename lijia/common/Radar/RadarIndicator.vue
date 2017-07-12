e9fc266ee76dfa34e921614353763eee95bc9a03
shepard <xia8910168@Hotmail.com>
2017-07-12 10:05:30 +0800

    修复缩放显示问题

--- lijia/common/Radar/RadarIndicator.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div style="position: absolute;left: 60px;top: 0;bottom: 0;right: 0" class="blue-grey darken-1">
-    <property-text label="是否显示坐标轴刻度"  option-key="radar.indicator[i].name" ></property-text>
-    <divider/>
-    <property-number label="刻度位置"  unit="px" option-key="radar.indicator[i].max"></property-number>
-    <property-number label="刻度长度"  unit="px" option-key="radar.indicator[i].min"></property-number>
-  </div>
-</template>
-
--- src/components/Layout/src/FormInputLayout.js
+++
@@ -1,9 +0,0 @@
-export default{
-  functional: true,
-  name: 'FormInput',
-  render(h, {data, props, children, listeners}){
-    return ( <div class="form-input">
-
-    </div> )
-  }
-}
