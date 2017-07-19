01118527389731e773c2829c6f170566cebb4b42
windjack <730087236@qq.com>
2017-07-19 16:48:40 +0800

    修改属性文件：图片上传 修改图片组件：使用上传图片属性

--- src/components/BoardEditor/Properties/index.js
+++ src/components/BoardEditor/Properties/index.js
@@ -2,12 +2,16 @@ import PropColor from './src/PropColor.vue'
 import PropTextarea from './src/propTextarea.vue'
 import propNumber from './src/propNumber.vue'
 import propSelect from './src/propSelect.vue'
-import propGroup from './src/propGroup.vue'
+import PropFontGroup from './src/PropFontGroup.vue'
+import PropUpload from './src/PropUpload.vue'
+import PropSlider from './src/PropSlider.vue'
 
 export {
   PropColor,
   PropTextarea,
   propNumber,
   propSelect,
-  propGroup,
+  PropFontGroup,
+  PropUpload,
+  PropSlider,
 }
