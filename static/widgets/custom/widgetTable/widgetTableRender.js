812c8016f1d3290cab736adf07385001bb6ad9d0
windjack <730087236@qq.com>
2017-07-27 18:03:42 +0800

    优化：表格组件的属性

--- static/widgets/custom/widgetTable/widgetTableRender.js
+++ static/widgets/custom/widgetTable/widgetTableRender.js
@@ -10,13 +10,7 @@ widgetTable.prototype.init =function(domID) {
 }
 
 widgetTable.prototype.render =function(domID,option) {
-  /*var element = document.getElementById(domID),
-    svgList =  element.getElementsByTagName("svg"),
-    size = svgList.length;
-    for(var i=0;i<size;i++){
-      element.removeChild(svgList[i])
-    }*/
-  createTable(domID,option);
+  new createTable(domID,option);
 }
 
 widgetTable.prototype.resize = function () {
