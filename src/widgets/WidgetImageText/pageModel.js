8d0a666f9cbaa312d864dfb5a2e661743787a607
windjack <730087236@qq.com>
2017-08-01 11:05:38 +0800

    添加图文组件

---
+++ src/widgets/WidgetImageText/pageModel.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 2017/7/27.
+ */
+export default {
+  WidgetTable: {
+    active: 'Base',
+    pages: [{
+      title: '基础',
+      name: 'Base',
+      active: 'widgetImageText',
+      pages: [{title: '属性', name: 'widgetImageText', component: 'ImageTextBasic'}]
+    }]
+  }
+}
--- src/widgets/WidgetTable/render.js
+++ src/widgets/WidgetTable/render.js
@@ -19,14 +19,12 @@ export default class WidgetTableRender extends Render {
   render (option) {
     this.createTable(this.el,option)
   }
-
-
   createTable(id,option){
-    var tableStyle="",rowStyle="",thStyle="";
-    var tOpt=option.tableStyle;//表格样式配置参数
-    var thOpt=option.thStyle;//表头样式配置参数
-    var rOpt=option.rowStyle;//行样式配置参数
-    for(var style in thOpt){//添加表头样式
+    let tableStyle="",rowStyle="",thStyle="";
+    let tOpt=option.tableStyle;//表格样式配置参数
+    let thOpt=option.thStyle;//表头样式配置参数
+    let rOpt=option.rowStyle;//行样式配置参数
+    for(let style in thOpt){//添加表头样式
       if(style=="height"||style=="font-size"){
         thStyle+=style+":"+thOpt[style]+"px;";
       }else if(style==="backgroundColor"){
@@ -35,13 +33,13 @@ export default class WidgetTableRender extends Render {
         thStyle+=style+":"+thOpt[style]+";";
       }
     }
-    var ths="";//存放表头行的单元格
-    var series=option.series;
-    var bw="border-width";
-    var bc="border-color";
-    var bs="border-style";
-    for(var i=0;i<series.length;i++){//遍历列数据，添加列
-      var style="",width="",align="";
+    let ths="";//存放表头行的单元格
+    let series=option.series;
+    let bw="border-width";
+    let bc="border-color";
+    let bs="border-style";
+    for(let i=0;i<series.length;i++){//遍历列数据，添加列
+      let style="",width="",align="";
       if(series[i].width){
         width="width:"+series[i].width+"px;";
       }
@@ -52,20 +50,20 @@ export default class WidgetTableRender extends Render {
       if(rOpt.borderCol){
         style+="border-right:"+rOpt[bw]+"px "+rOpt[bs]+" "+rOpt[bc]+";";
       };
-      var div=`<div class="cell">${series[i].text}</div>`;
-      var th=`<th style="${style}">${div}</th>`;
+      let div=`<div class="cell">${series[i].text}</div>`;
+      let th=`<th style="${style}">${div}</th>`;
       ths+=th;
     }
     //创建table表格的主体内容
-    var bTrs="";//用于存放表主体的行
-    var tdData=option.data;
-    var rows=option.data.length;
-    for(var r=1;r<=rows;r++){//遍历表格数据，添加行
+    let bTrs="";//用于存放表主体的行
+    let tdData=option.data;
+    let rows=option.data.length;
+    for(let r=1;r<=rows;r++){//遍历表格数据，添加行
       //创建tr
       rowStyle="";
-      var bg="background-color";
-      for(var style in rOpt){//行样式设置
-        var val=rOpt[style];
+      let bg="background-color";
+      for(let style in rOpt){//行样式设置
+        let val=rOpt[style];
         if(style.indexOf("border")==-1&&style!="oddColor"&&style!="evenColor"&&style!="background-color"){
           if(style=="height"||style=="font-size"){
             rowStyle+=style+":"+val+"px;";
@@ -91,10 +89,10 @@ export default class WidgetTableRender extends Render {
           rowStyle+="background-color:"+rOpt[bg];
         }
       }
-      var tds="";//存放表主体行的单元格
-      for(var i=0;i<series.length;i++){
+      let tds="";//存放表主体行的单元格
+      for(let i=0;i<series.length;i++){
         //创建td
-        var tdStyle="",colStyle="",w="",a="";
+        let tdStyle="",colStyle="",w="",a="";
         if(series[i].style){//列其他样式添加
           colStyle=series[i].style;
         }
@@ -109,15 +107,15 @@ export default class WidgetTableRender extends Render {
         if(rOpt.borderCol){
           tdStyle+="border-right:"+rOpt[bw]+"px "+rOpt[bs]+" "+rOpt[bc]+";";
         };
-        var div=`<div class="cell">${tdData[r-1][i]}</div>`;
-        var td=`<td style="${tdStyle?tdStyle:""}">${div}</td>`;
+        let div=`<div class="cell">${tdData[r-1][i]}</div>`;
+        let td=`<td style="${tdStyle?tdStyle:""}">${div}</td>`;
         tds+=td;
       }
-      var bTr=`<tr style="${rowStyle?rowStyle:""}">${tds}</tr>`;
+      let bTr=`<tr style="${rowStyle?rowStyle:""}">${tds}</tr>`;
       bTrs+=bTr;
     }
-    for (var style in tOpt){//添加表格样式
-      var valT=tOpt[style];
+    for (let style in tOpt){//添加表格样式
+      let valT=tOpt[style];
       if(style!="border"&&style!="align"){
         if(style=="height"||style=="border-width"||style=="font-size"){
           tableStyle+=style+":"+valT+"px;";
@@ -130,13 +128,13 @@ export default class WidgetTableRender extends Render {
       tableStyle+="border:none;"
     };
     //全局设置文本水平对齐方式
-    var tableClass="";
+    let tableClass="";
     if(tOpt.align==="center"){
       tableClass="alignCenter";
     }else if(tOpt.align==="right"){
       tableClass="alignRight";
     }
-    var renderHtml=`
+    let renderHtml=`
             <div class="ydp-table-wrapper" >
                <table class="ydp-table ${tableClass?tableClass:""}" width="100%" border="0" cellspacing="0" cellpadding="0" style="${tableStyle?tableStyle:""}">
                 <thead>
