9e0e586b0006bd33cfe15442727755a31d759495
windjack <730087236@qq.com>
2017-08-03 10:41:36 +0800

    修改数据库的渲染方法

--- src/widgets/WidgetDataBlock/render.js
+++ src/widgets/WidgetDataBlock/render.js
@@ -19,11 +19,39 @@ export default class WidgetTableRender extends Render {
   render (option) {
     this.dataBlock(this.el,option)
   }
-  dataBlock(id,option) {
+  dataBlock(id,option) {//数据块渲染方法
     let parent=document.getElementById(id);
-
-    let renderHtml=``;
-    parent.innerHTML=renderHtml;
+    let rows= option.rows;
+    let len = option.rows.length;
+    let data=option.data;
+    let commonStyle="",lis="";
+    commonStyle=this.getStyle(option.style,commonStyle);
+    for(let i=0;i<len;i++){
+      let liStyle="",dataStyle="",titleStyle="",unitStyle="";
+      liStyle=commonStyle+this.getStyle(rows[i].rowStyle,liStyle);
+      titleStyle=this.getStyle(rows[i].titleStyle,titleStyle);
+      dataStyle=this.getStyle(rows[i].dataStyle,dataStyle);
+      unitStyle=this.getStyle(rows[i].unitStyle,unitStyle);
+      let li=`
+            <li style="${liStyle}">
+                <span class="dataBlock_title" style="${titleStyle}">${rows[i].titleStyle.text}</span>
+                <span class="dataBlock_data" style="${dataStyle}">${data[i]}</span>
+                <span class="dataBlock_unit" style="${unitStyle}">${rows[i].unitStyle.text}</span>
+            </li>`;
+      lis+=li;
+    }
+    let html=`<ul class="ydp-dataBlock">${lis}</ul>`;
+    parent.innerHTML=html;
+  }
+  getStyle(obj,style) {//获取样式
+    for(let key in obj){
+      let val=obj[key];
+      if(typeof val === 'number'){
+        style+=key+":"+val+"px;";
+      }else if(key!=="text"){
+        style+=key+":"+val+";";
+      }
+    }
+    return style;
   };
-
 }
