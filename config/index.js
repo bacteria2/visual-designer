a26153cbc5e9d32e9fc78def71fb8d401f82662e
shepard <xia8910168@Hotmail.com>
2017-08-10 17:41:25 +0800

    独立share模块

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../dist/visual/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist/visual'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
--- src/pages/HomePage/Home.vue
+++ src/pages/HomePage/Home.vue
@@ -4,23 +4,23 @@
       <mu-raised-button class="collapse-btn" style="" slot="logo" @click="menuCollapsed=!menuCollapsed">
         <mu-icon :value="iconName" color="white"></mu-icon>
       </mu-raised-button>
-      <mu-raised-button class="collapse-btn" slot="rightEnd">
-        <mu-icon value="person" color="white"></mu-icon>
+      <mu-raised-button class="collapse-btn" slot="rightEnd" @click="onHomeClick" style="margin-right: 15px">
+        <mu-icon value="home" color="white"></mu-icon>
       </mu-raised-button>
-      <mu-raised-button class="collapse-btn" slot="rightEnd" @click="logout">
+      <mu-raised-button class="collapse-btn" slot="rightEnd" @click="onExitClick">
         <mu-icon value="power_settings_new" color="white"></mu-icon>
       </mu-raised-button>
     </view-header>
     <aside class="side_menu" :class="{collapsed:menuCollapsed}">
       <mu-list v-if="!menuCollapsed">
         <template v-for="item in menu">
-          <mu-list-item v-if="!item.children" :title="item.title" :href='item.url' target="iFrame">
+          <mu-list-item v-if="!item.children" :title="item.title" :href='item.url' target="iFrame" @click="activeUrl=item.url" :class="{'router-link-active':activeUrl===item.url}">
             <mu-icon slot="left" :value="item.icon"></mu-icon>
           </mu-list-item>
-
-          <mu-list-item v-else :title="item.title" toggleNested>
+          <mu-list-item v-else :title="item.title" toggleNested :open="item.open">
             <mu-icon slot="left" :value="item.icon"></mu-icon>
             <mu-list-item class="nest_menu" v-for="child,index in item.children" slot="nested"
+                          @click="activeUrl=child.url" :class="{'router-link-active':activeUrl===child.url}"
                           :title="child.title" target="iFrame"
                           :href="child.url" :key="index">
               <mu-icon slot="left" :value="child.icon"></mu-icon>
@@ -32,8 +32,8 @@
       <div v-else>
         <ul class="menu_list ">
           <template v-for="item in menu">
-            <li :class="!!item.children?'menu_submenu':'menu_item'">
-              <a v-if="!item.children" :href="item.url" target="iFrame">
+            <li :class="[!!item.children?'menu_submenu':'menu_item']"  >
+              <a v-if="!item.children" :href="item.url" target="iFrame" @click="activeUrl=item.url" :class="{'router-link-active':activeUrl===item.url}">
                 <mu-flat-button style="width: 56px;height:48px;min-width: 56px;color:#757575" :icon="item.icon"></mu-flat-button>
               </a>
               <template v-else>
@@ -42,8 +42,8 @@
                   <span>{{item.title}}</span>
                 </div>
                 <ul class="menu_list menu_sub">
-                  <li class="menu_item" v-for="child in item.children">
-                    <a :href="child.url" target="iFrame">
+                  <li class="menu_item" v-for="child in item.children"  >
+                    <a :href="child.url" target="iFrame" @click="activeUrl=child.url" :class="{routerLinkActive:activeUrl===child.url}">
                       <mu-flat-button style="width: 200px;height:48px;min-width: 200px;color:#757575" :label="child.title"></mu-flat-button>
                     </a>
                   </li>
@@ -66,11 +66,13 @@
   @import "../../style/common/header";
 </style>
 <style>
-  @import '../../../node_modules/muse-ui/dist/muse-ui.css';
-  @import '../../style/third/material-Icons/material-icons.css';
+  @import '../../style/third/material-icon/material-icons.css';
 </style>
 <script>
   import {Header} from '../../views/common'
+  import '../../../node_modules/muse-ui/dist/muse-ui.css';
+
+
 
   export default{
     components: {
@@ -80,56 +82,26 @@
     props:{
       menuList:{
         type:Array,
-        /*default(){
-          return  [
-            {
-              title: "我的驾驶舱",
-              url: "/dashboard/list",
-              icon: "dashboard"
-            },
-            {
-              title: "我的组件",
-              url: "/widget/list",
-              icon: "face"
-            },
-            {
-              title: "组件库",
-              url: "/origin/list",
-              icon: "assessment"
-            },
-            {
-              title: "数据源管理",
-              url: "/datasource/list",
-              icon: "dns"
-            },
-            {
-              title: "素材管理",
-              url: "/raw/list",
-              icon: "collections"
-            },
-            {
-              title: "设置",
-              icon: "build",
-              divider: true,
-              children: [{
-                title: "驾驶舱分类",
-                url: "/user/list",
-                icon: "view_list",
-              },
-                {
-                  title: "应用分类",
-                  url: "/appComp/list",
-                  icon: "view_headline",
-                }
-              ]
-            }
-          ]
-        }*/
+        default(){
+          return  []
+        }
       },
       homePage:{type:String,default:"http://www.163.com"},
       title:{
         type:String,
         default:"Some String"
+      },
+      onHomeClick:{
+        type:Function,
+        default(){
+          console.log('home click')
+        }
+      },
+      onExitClick:{
+        type:Function,
+        default(){
+          console.log('exit click')
+        }
       }
     },
     computed: {
@@ -138,11 +110,10 @@
       }
     },
     data(){
-      console.log(this.menuList)
-     let  menu=this.menuList;
       return {
-        menu,
-        menuCollapsed: false
+        menu:this.menuList,
+        menuCollapsed: false,
+        activeUrl:this.homePage,
       }
     },
     methods: {
