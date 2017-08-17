402bb1745778ddd2e590488dbe43da6e98d9d88c
k24 <winsuntom@163.com>
2017-08-17 20:06:35 +0800

    修改样式

--- src/views/widgetList/widget.vue
+++ src/views/widgetList/widget.vue
@@ -320,6 +320,8 @@
           if (widget.fDataOption == '') {
             widget.fDataOption = def.fDataOption
           }
+        }else{
+          widget.fDataOption = '{"dataSet":[],"dimension":[]}'
         }
         if (widget.showSetting && widget.showSetting.includes('series')) {
           store.commit("loadShowSetting", {sSetting: widget.showSetting});
@@ -354,7 +356,7 @@
       save(){
           let wg = this.widget;
           wg.showSetting = JSON.stringify(store.getters.getShowSetting)
-          if(wg.fDynamic.trim() == ""){
+          if(wg.fDynamic == ""){
             wg.fDynamic = 0
           }
           saveWidget({widgetsVO: wg, thumbnail: this.thumbnail}).then((resp) => {
